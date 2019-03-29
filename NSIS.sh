7c27cc86d5c2bbe947aff96336a8e277d9aebf1e
NimrakMill <49028294+NimrakMill@users.noreply.github.com>
2019-03-29 16:08:38 +0700

    Add files via upload

---
+++ NSIS.sh
@@ -0,0 +1,251 @@
+===================
+clear
+
+figlet -f slant Installer Tool
+
+echo "========================================" | lolcat
+echo "Tools By NimrakMill" | lolcat
+echo "Tools Installer" | lolcat
+echo "Team : CYBER N.S.I.S (Nsis)" | lolcat
+echo "========================================" | lolcat
+echo
+echo "==================" | lolcat
+echo "1. Installer BAJINGAN"
+echo "=================="
+echo "2.Installer TU4N G4L4U"
+echo "=================="
+echo "3.Install Spam"
+echo "=================="
+echo "4.Installer CrewBot"
+echo "=================="
+echo "5.install MBF"
+echo "=================="
+echo "6.Hack Cctv"
+echo "=================="
+echo ".7 installer TUANB4DUT"
+echo "=================="
+echo ".8 Hack instagram"
+echo "=================="
+echo ".9 Tembax Xl"
+echo "=================="
+echo ".10 Tes Kecepatan Jaringan"
+echo "=================="
+echo ".11 Hack FB DLL"
+echo "=================="
+echo ".12 Spamm WA"
+echo ".================="
+echo ".13 200+ Tools"
+echo "=================="
+echo ".14 Install OSIF"
+echo "=================="
+read -p "r00t@NimrakMill~# " pill
+if [ $pill = 1 ]
+then
+clear
+echo "Tungguu Kontoeell....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/DarknessCyberTeam/...
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+
+if [ $pill = 2 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/TU4NG4L4U/InstallerV6
+cd InstallerV6
+sh InstallerV6.sh
+fi
+
+
+fi [ $pill =3 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/mrmsdv/sdvspam.git
+cd sdvspam
+sh install.sh
+sh sdv.sh
+fi
+
+
+fi [ $pill =4 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/Xeit666h05t/CrewBot
+ls
+cd CrewBot
+python2 CrewBot.pyc
+fi
+
+
+fi [ $pill =5 ]
+then
+clear
+echo "Tunggu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+
+
+fi [ $pill =6 ]
+then
+clear
+echo "Tunguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+pip2 install requests
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+
+fi [ $pill =7 ]
+then
+clear
+echo "Tunggu Kontoeell.....1!1!1"
+echo
+sleep 1
+apt install git
+apt install php
+apt install curl
+apt install ruby
+apt install figlet
+apt install python2
+gem install lolcat
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh
+fi
+
+fi [ $pill =8 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install nano
+pkg install git
+pkg install nano
+git clone https://github.com/avramit/instahack.git
+ls
+cd instahack
+ls
+pip install requests
+cd instahack
+nano pass.txt
+cat pass.txt
+ls
+python hackinsta.py
+fi
+
+fi [ $pill =9 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+pkg install python
+git clone https://github.com/albertoanggi/xl-py
+pip install -r requirements.txt
+chmod +x app.py
+python2 app.py
+fi
+
+fi [ $pill =10 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+pkg install requests
+pkg install python2
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+python2 Speed.py
+fi
+
+fi [ $pill =11 ]
+then
+clear
+echo "Tunguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+pkg ins toilet
+pkg ins figlet
+pkg ins ruby
+pkg ins gem
+gem ins lolcat
+pkg ins php
+pkg ins python2
+pkg ins git
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+fi
+
+fi [ $pill =12 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install php
+pkg install git -y
+git clone https://github.com/siputra12/prank
+cd prank
+php wa.php
+fi
+
+fi [ $pill =13 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+apt install bash
+apt install git
+git clone https://github.com/21D4N404/tools-installer.git
+cd tools-installer
+sh install.sh
+fi
+
+fi [ $pill =14 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+apt install python2
+pip2 install requests mechanize
+apt install git
+git clone $https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+==================
